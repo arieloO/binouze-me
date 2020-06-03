9e5298b9254ab31bb87af808b46b2b1a2fd83cfd
Ariel Levy <8zF-KAg-kkJ-HYD>
2020-06-03 14:34:33 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/binouze-me/precache-manifest.b3d780052411b798d0ec1c54b74534fe.js"
+  "/binouze-me/precache-manifest.ae0b3b37e6bed1ca92489f6721e9fc32.js"
 );
 
 self.addEventListener('message', (event) => {
