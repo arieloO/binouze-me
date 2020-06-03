558501cdd37dedb256185f6dc508c7db16c26b1c
Ariel Levy <8zF-KAg-kkJ-HYD>
2020-06-03 15:43:15 +0200

    Updates

--- precache-manifest.613bd13d570238172c71895955c63145.js
+++ precache-manifest.613bd13d570238172c71895955c63145.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aa510a0e93b0350b33647b666b3516e8",
+    "revision": "c675679a098b3656f49adc017c4aecf2",
     "url": "/binouze-me/index.html"
   },
   {
-    "revision": "9f1ca44ee991262d065f",
-    "url": "/binouze-me/static/css/main.60943fa2.chunk.css"
+    "revision": "6314259ef0a077645f18",
+    "url": "/binouze-me/static/css/main.8561d377.chunk.css"
   },
   {
     "revision": "06bc9037e6a6300d4d4b",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/binouze-me/static/js/2.1d62740f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9f1ca44ee991262d065f",
-    "url": "/binouze-me/static/js/main.0d0342f0.chunk.js"
+    "revision": "6314259ef0a077645f18",
+    "url": "/binouze-me/static/js/main.eba72dcb.chunk.js"
   },
   {
     "revision": "f3995f14043257ea7ff8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/binouze-me/precache-manifest.ae0b3b37e6bed1ca92489f6721e9fc32.js"
+  "/binouze-me/precache-manifest.613bd13d570238172c71895955c63145.js"
 );
 
 self.addEventListener('message', (event) => {
