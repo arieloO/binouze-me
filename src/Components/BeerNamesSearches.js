e4ee78ba18d0e4023fbf71538ef1b255c855ba25
Ariel Levy <ariel.levyk@gmail.com>
2020-06-25 11:48:24 +0200

    Trying to copy accordion drop-down filter from famous shoes webSite

--- src/Components/BeerNamesSearches.js
+++ src/Components/BeerNamesSearches.js
@@ -17,24 +17,19 @@ const BeerNamesSearches = (props) => {
 
   return (
     <div className="filter">
+      <div
+        className={open ? "filter-option open" : "filter-option hiden"}
+        key={index}
+        onClick={() => {
+          setOpen(!open);
+          console.log(open);
+        }}
+      >
+        <h3 className="option-title"> type of beer :</h3>
+      </div>
       <ul>
-        <li
-          className="filter-option"
-          onClick={() => {
-            setOpen(!open);
-            console.log(open);
-          }}
-        >
-          <h3>type of beer :</h3>
-        </li>
         {beerTypes.map((beerType, index) => (
-          <li
-            className="filter-option"
-            style={{
-              display: open ? "inherit" : "none",
-            }}
-            key={index}
-          >
+          <li className="filter-option-list" key={index}>
             <input
               type="radio"
               id={beerType}
