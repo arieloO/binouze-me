00db8d26030b17dcde0e19bfa3092e873ee23674
Ariel Levy <ariel.levyk@gmail.com>
2021-01-18 17:24:55 +0100

    fixed navFilters overflow, nesting filters in new container div with overflowscrool, made option-list-title sticky and added background, fixed radio inputs on each option li to display-block to fix alignment. Style to first and last child of option list ul. More side margin on previous/next arrows.

--- src/Components/Filters.js
+++ src/Components/Filters.js
@@ -23,60 +23,61 @@ const ListFilters = ({
         <h3 hidden={hidden}>Filters :</h3>
         <FilterIcon id={hidden ? "filter-icon-hidden" : "filter-icon"} />
       </div>
-      <div className={hidden ? "filter-hidden" : "filter"}>
-        {" "}
-        Alcohol by volume :
-        <MultiSliderTest
-          rangeDomain={abvDomain}
-          range={abvRange}
-          handleRangeChange={(abv) => {
-            onChange(abv, ibuRange, srmRange);
-          }}
-          ticksNumber={6}
-        />
-      </div>
-      <div className={hidden ? "filter-hidden" : "filter"}>
-        Bitterness (IBU) :
-        <MultiSliderTest
-          rangeDomain={ibuDomain}
-          range={ibuRange}
-          handleRangeChange={(ibu) => {
-            onChange(abvRange, ibu, srmRange);
-          }}
-          ticksNumber={6}
-          // sliderBackgroundColor={}
-        />
-      </div>
-      <div className={hidden ? "filter-hidden" : "filter"}>
-        Color (SRM) :
-        <MultiSliderTest
-          rangeDomain={srmDomain}
-          range={srmRange}
-          handleRangeChange={(srm) => {
-            onChange(abvRange, ibuRange, srm);
-          }}
-          ticksNumber={6}
-          railBackgroundColor={SRMColorRadient}
-          noTrack={true}
-          customHandleColor={true}
-          customColorsList={SRMcolor}
-        />
-      </div>
+      <div id="filters-container">
+        <div className={hidden ? "filter-hidden" : "filter"}>
+          {" "}
+          Alcohol by volume :
+          <MultiSliderTest
+            rangeDomain={abvDomain}
+            range={abvRange}
+            handleRangeChange={(abv) => {
+              onChange(abv, ibuRange, srmRange);
+            }}
+            ticksNumber={6}
+          />
+        </div>
+        <div className={hidden ? "filter-hidden" : "filter"}>
+          Bitterness (IBU) :
+          <MultiSliderTest
+            rangeDomain={ibuDomain}
+            range={ibuRange}
+            handleRangeChange={(ibu) => {
+              onChange(abvRange, ibu, srmRange);
+            }}
+            ticksNumber={6}
+            // sliderBackgroundColor={}
+          />
+        </div>
+        <div className={hidden ? "filter-hidden" : "filter"}>
+          Color (SRM) :
+          <MultiSliderTest
+            rangeDomain={srmDomain}
+            range={srmRange}
+            handleRangeChange={(srm) => {
+              onChange(abvRange, ibuRange, srm);
+            }}
+            ticksNumber={6}
+            railBackgroundColor={SRMColorRadient}
+            noTrack={true}
+            customHandleColor={true}
+            customColorsList={SRMcolor}
+          />
+        </div>
 
-      <div className={hidden ? "filter-hidden" : "filter"}>
-        <BeerTypeFilter
-          key="1"
-          handleSwitch={handleNameSearch}
-          category="Beer"
-          typesArray={beerTypes}
-        />
-        <BeerTypeFilter
-          key="2"
-          handleSwitch={handleNameSearch}
-          category="Yeast"
-          typesArray={yeastTypes}
-        />
-        {/* <div
+        <div className={hidden ? "filter-hidden" : "filter"}>
+          <BeerTypeFilter
+            key="1"
+            handleSwitch={handleNameSearch}
+            category="Beer"
+            typesArray={beerTypes}
+          />
+          <BeerTypeFilter
+            key="2"
+            handleSwitch={handleNameSearch}
+            category="Yeast"
+            typesArray={yeastTypes}
+          />
+          {/* <div
             id="brown"
             style={{
               height: 100,
@@ -84,6 +85,7 @@ const ListFilters = ({
               background: `linear-gradient(100deg, ${hexForSRMs[0]},${hexForSRMs[3]}, ${hexForSRMs[2]}, ${hexForSRMs[1]})`,
             }}
           ></div> */}
+        </div>
       </div>
     </div>
   );
