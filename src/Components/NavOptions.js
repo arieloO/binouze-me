f0b4f800efdf0769efb086eeb37e6924b6f74ca7
Ariel <ariel.levyk@gmail.com>
2021-03-06 17:35:47 +0100

    removed page arrows and itemsPerPage select input from NavLink. Cleared unusefull  Props

--- src/Components/NavOptions.js
+++ src/Components/NavOptions.js
@@ -5,28 +5,14 @@ import { ReactComponent as SmallItemsIcon } from "../media/small-items-icon.svg"
 import { ReactComponent as LargeItemsIcon } from "../media/large-items-icon.svg";
 
 const NavOptions = ({
-  beers,
-  itemsPage,
-  page,
-  pageDisplay,
-  onChange,
   largeGrid,
   switchGrid,
   hiddenFilters,
   handleHiddenFilters,
   isMobile,
 }) => {
-  // console.log("hidden filters :::: ", hiddenFilters);
-
   return (
     <div className="nav-options">
-      {/* <button
-        onClick={() => onChange(page - 1, itemsPage)}
-        className="page-nav"
-        disabled={page === 1 ? true : false}
-      >
-        ⇦
-      </button> */}
       <div
         className="nav-filter-title"
         hidden={!isMobile}
@@ -34,20 +20,6 @@ const NavOptions = ({
       >
         <FilterIcon id="filter-icon" />
       </div>
-      <span>{pageDisplay}</span>
-      <div>
-        <select
-          id="itemsPerPage"
-          value={itemsPage}
-          onChange={(e) => onChange(1, parseInt(e.target.value))}
-        >
-          <option value={25}>25</option>
-          <option value={50}>50</option>
-          <option value={75}>75</option>
-        </select>
-        <label htmlFor="itemsPerPage">items</label>
-      </div>
-      {/* <button > */}
       <div className="items-size-button" onClick={() => switchGrid(largeGrid)}>
         <SmallItemsIcon
           className={!largeGrid ? "grid-icon-selected grid-icon" : "grid-icon"}
@@ -56,7 +28,6 @@ const NavOptions = ({
           className={largeGrid ? "grid-icon-selected grid-icon" : "grid-icon"}
         />
       </div>
-      {/* </button> */}
       <div>
         <NavLink
           to="/catalogue/beers/id=random"
@@ -69,13 +40,6 @@ const NavOptions = ({
           />
         </NavLink>
       </div>
-      {/* <button
-        onClick={() => onChange(page + 1, itemsPage)}
-        className="page-nav"
-        disabled={beers.length !== itemsPage ? true : false}
-      >
-        ⇨
-      </button> */}
     </div>
   );
 };
